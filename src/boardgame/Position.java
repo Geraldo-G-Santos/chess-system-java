5a3a9af7c170630fab3b315684f6be57810c26e6
Geraldo <geraldo.gsantos@yahoo.com.br>
2022-07-16 23:37:00 -0300

    Trocando de jogador a cada turno

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,15 +1,15 @@
 package boardgame;
 
 public class Position {
-
+	//variaveis privadas a essa classe Position//
 	private int row;
 	private int column;
-	
+	// 
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
-
+	
 	public int getRow() {
 		return row;
 	}
